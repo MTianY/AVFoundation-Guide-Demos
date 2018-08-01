ff38ea4bca13532d9917d529c3b7f7a32c4614ce
马天野 <maty@matydeimac.local>
2018-08-01 09:14:45 +0800

    初始化 VoiceMemo

---
+++ 3. 录制音频/VoiceMemoDemo/VoiceMemoDemo/TYVoiceMemoViewController.m
@@ -0,0 +1,29 @@
+//
+//  TYVoiceMemoViewController.m
+//  VoiceMemoDemo
+//
+//  Created by Maty on 2018/8/1.
+//  Copyright © 2018年 kangarootec. All rights reserved.
+//
+
+#import "TYVoiceMemoViewController.h"
+
+@interface TYVoiceMemoViewController ()
+
+@end
+
+@implementation TYVoiceMemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupUI];
+}
+
+#pragma mark - UI
+- (void)setupUI {
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+#pragma mark - Lazy Load
+
+@end
